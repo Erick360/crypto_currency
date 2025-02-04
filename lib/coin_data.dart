2e40674c8acec612d9fb2325623c0c30e522a57a
Eri Romero Perez <eri360roperez@gmail.com>
2025-02-03 22:18:50 -0600

    first update

---
+++ lib/coin_data.dart
@@ -0,0 +1,28 @@
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoLidt = [
+  'BTC',
+  'ETH',
+  'LTC'
+];
